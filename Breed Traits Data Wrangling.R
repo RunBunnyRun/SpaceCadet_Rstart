631e7f17f1784766050a33234240c3935b1de7ba
RunBunnyRun <kiwinyx@gmail.com>
2022-12-02 16:51:35 +1300

    Practise with pipe and arrange.

--- Breed Traits Data Wrangling.R
+++ Breed Traits Data Wrangling.R
@@ -1,12 +1,15 @@
 # install.packages("tidytuesdayR")
 #install.packages("dplyr")
 #install.packages("janitor)
+
 library(janitor)
 library(dplyr)
-#tuesdata <- tidytuesdayR::tt_load("2022-02-01")
-#saveRDS(tuesdata$breed_traits, "breed_traits.rds")
+
+# tuesdata <- tidytuesdayR::tt_load("2022-02-01")
+# saveRDS(tuesdata$breed_traits, "breed_traits.rds")
 breed_traits <- clean_names(readRDS("breed_traits.rds"))
-#breed_traits <- tuesdata$breed_traits
+# breed_traits <- tuesdata$breed_traits
+
 select(breed_traits, "Breed")
 select(breed_traits, barking_level)
 select(breed_traits, "Coat Type")
@@ -17,6 +20,7 @@ select(breed_traits, 1,2,6:10)
 select(breed_traits, "Breed", "Affectionate With Family", "Drooling Level":"Openness To Strangers")
 select(breed_traits, breed, shedding_level, coat_grooming_frequency)
 #testing clean_names function on selecting columns and running filters
+
 filter(breed_traits, drooling_level == 5)
 filter(breed_traits, drooling_level == 5 & coat_length == "Short")
 filter(breed_traits, drooling_level == 5 | drooling_level == 3)
@@ -24,16 +28,32 @@ filter(breed_traits, drooling_level %in% c(1,3,5))
 filter(breed_traits, drooling_level %in% c(3,4,5) & coat_length == "Short")
 filter(breed_traits, drooling_level != 5)
 filter(breed_traits, !drooling_level %in% c(1,3,5))
-#different ways to filter, including a NOT operator (!). Homework is to choose Dog Breeds that meet all 3 of:
+#different ways to filter, including a NOT operator (!).
+
+# Homework is to choose Dog Breeds that meet all 3 of:
 #Most affectionate with family (4,5)
 #Best with Young Children (4,5)
 #Best with Other Dogs (4,5)
 #Then save output to an object called "friendly_dogs"
+
 #filter(breed_traits, affectionate_with_family == 4 | affectionate_with_family == 5)
 #filter(breed_traits, affectionate_with_family == 4 | affectionate_with_family == 5 & good_with_young_children == 4 | good_with_young_children == 5 & good_with_other_dogs == 4 | good_with_other_dogs == 5)
 #filter(breed_traits, affectionate_with_family %in% c(1,2,3) & good_with_young_children %in% c(1,2,3) & good_with_other_dogs %in% c(1,2,3))
 #filter(breed_traits, affectionate_with_family %in% c(1,2,3), good_with_young_children %in% c(1,2,3), good_with_other_dogs %in% c(1,2,3))
 filter(breed_traits, affectionate_with_family %in% c(4,5), good_with_young_children %in% c(4,5), good_with_other_dogs %in% c(4,5))
 #The above code seems to give me the correct list.
+
 friendly_dogs <- filter(breed_traits, affectionate_with_family %in% c(4,5), good_with_young_children %in% c(4,5), good_with_other_dogs %in% c(4,5))
 friendly_dogs
+
+# arrange(breed_traits, breed) |> View()
+arrange(breed_traits, desc(breed))
+arrange(breed_traits, desc(breed), good_with_other_dogs)
+arrange(breed_traits, desc(coat_length), breed)
+# Testing different ways to arrange.
+
+breed_traits |>
+  filter(drooling_level == 5) |> 
+  arrange(breed)
+# Testing pipe - filter than arrange via the pipe.
+  
